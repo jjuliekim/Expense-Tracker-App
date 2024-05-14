df40dc25e3b9c10e9af2a54a00d30efea9672517
jjuliekim <118621382+jjuliekim@users.noreply.github.com>
2024-05-14 14:40:32 -0400

    check login

--- app/src/main/java/com/example/kim_j_project2/LoginActivity.java
+++ app/src/main/java/com/example/kim_j_project2/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.kim_j_project2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,17 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // handle login process
+    public void checkLogin(View view) {
+        EditText userInput = view.findViewById(R.id.userInput);
+        String username = userInput.getText().toString();
+        EditText pwInput = view.findViewById(R.id.pwInput);
+        String password = pwInput.getText().toString();
+        // if username exists, check if matches password
+        // if incorrect password -> Toast ("Invalid Log In")
+        // if new username, save info to Shared Preferences -> Toast ("Registered")
+        // direct to dashboard activity
     }
 }
