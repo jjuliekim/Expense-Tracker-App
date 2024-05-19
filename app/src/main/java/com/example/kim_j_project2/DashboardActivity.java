c213c3ebb8883d83d6ca92ec34209448c46c5617
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-18 21:21:55 -0400

    log in debugging

--- app/src/main/java/com/example/kim_j_project2/DashboardActivity.java
+++ app/src/main/java/com/example/kim_j_project2/DashboardActivity.java
@@ -21,6 +21,8 @@ import java.util.Objects;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    List<Expense> expenseList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +33,7 @@ public class DashboardActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        List<Expense> expenseList;
+
         Intent myIntent = getIntent();
         String username = myIntent.getStringExtra("username");
 
