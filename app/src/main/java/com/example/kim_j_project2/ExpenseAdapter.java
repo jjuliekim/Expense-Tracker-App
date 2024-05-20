536751d82b9fd11c57423d9ca9725b6705bedebf
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-20 19:58:05 -0400

    expense adapter and xml

---
+++ app/src/main/java/com/example/kim_j_project2/ExpenseAdapter.java
@@ -0,0 +1,30 @@
+package com.example.kim_j_project2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ExpenseAdapter extends ArrayAdapter<Expense> {
+
+    public ExpenseAdapter(Context context, List<Expense> expenseList) {
+        super(context, 0, expenseList);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Expense expense = getItem(position);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.expense_item, parent, false);
+
+        TextView nameText = convertView.findViewById(R.id.exp_name);
+        TextView amtText = convertView.findViewById(R.id.exp_amt);
+        nameText.setText(expense.getExpenseName());
+        amtText.setText(String.valueOf(expense.getExpenseAmt()));
+
+        return convertView;
+    }
+}
