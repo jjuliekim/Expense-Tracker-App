4f012eec497057efd36549fa5b29eb029f02c9d2
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-05-20 20:15:11 -0400

    expense list

--- app/src/main/java/com/example/kim_j_project2/ExpenseListActivity.java
+++ app/src/main/java/com/example/kim_j_project2/ExpenseListActivity.java
@@ -1,12 +1,21 @@
 package com.example.kim_j_project2;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 
 public class ExpenseListActivity extends AppCompatActivity {
@@ -21,6 +30,8 @@ public class ExpenseListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadList();
     }
 
     @Override
@@ -31,6 +42,22 @@ public class ExpenseListActivity extends AppCompatActivity {
 
     // display list of expenses
     private void loadList() {
+        Intent myIntent = getIntent();
+        String username = myIntent.getStringExtra("username");
 
+        // load expense list and use custom adapter
+        ArrayList<Expense> expenseList = JsonManager.loadExpenses(this, username);
+        ListView listView = findViewById(R.id.listView);
+        ExpenseAdapter adapter = new ExpenseAdapter(this, expenseList);
+        listView.setAdapter(adapter);
+
+        // set on click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Expense clickedExpense = (Expense) parent.getItemAtPosition(position);
+                Toast.makeText(ExpenseListActivity.this, "Clicked: " + clickedExpense.getExpenseName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
